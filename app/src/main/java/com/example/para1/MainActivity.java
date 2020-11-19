6641f811d0baeb11977b841b2331909a91922bd8
anastasiatarazevich <anastasiya.tarazevich22@gmail.com>
2020-11-19 18:59:56 +0300

    Start of the project

--- app/src/main/java/com/example/para1/MainActivity.java
+++ app/src/main/java/com/example/para1/MainActivity.java
@@ -2,49 +2,17 @@ package com.example.para1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.zip.CRC32;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText inputEditText;
-    EditText keytEditText;
-    EditText outputEditText;
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-         inputEditText = findViewById(R.id.input);
-         keytEditText = findViewById(R.id.key);
-        outputEditText = findViewById(R.id.result);
-
-    }
-
-    private String encrypt(String s, int key){
-
-        int m = 'Z' - 'A' + 1;
-        int offset = 'A';
-        StringBuilder output = new StringBuilder();
-
-        for(int i=0; i < s.length(); i++){
-            char y = (char) (((s.charAt(i) - offset + key) % m) + offset);
-            output.append(y);
-        }
-
-        return output.toString();
-    }
-
-    public void onCaesarButtonClick(View view){
-
-        String sInput = inputEditText.getText().toString().toUpperCase();
-        String sKey = keytEditText.getText().toString();
-        int key_int = Integer.parseInt(sKey);
-        outputEditText.setText(encrypt(sInput, key_int));
-
-    }
 }
